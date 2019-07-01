7def3dd854158c403375a0d02da964348eef82c2
MichaelDrake <41341651+MichaelDrake@users.noreply.github.com>
2019-06-30 19:17:14 -0500

    User login  Validations ok
    asdasd

--- Frontend-inmocasas/app/src/main/java/com/example/inmocasas/MainActivity.kt
+++ Frontend-inmocasas/app/src/main/java/com/example/inmocasas/MainActivity.kt
@@ -4,12 +4,14 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.beust.klaxon.Klaxon
+import com.example.inmocasas.dto.RolUsuario
 import com.example.inmocasas.dto.Usuario
 import com.example.inmocasas.services.http.HttpUsuario
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     var url = "http://192.168.56.1:1337/"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,56 +27,38 @@ class MainActivity : AppCompatActivity() {
         var usuario:HttpUsuario = HttpUsuario(url,"Usuario")
         var response = usuario.getByQuery("email=${edit_email.text}&contrasenia=${edit_password.text}")
         Log.i("login","El uSER ES ${response}")
-        jsonParse(response)
+        if(response == "[]"){
+            Log.i("ERROR","ERRORRRR")
+        }
+        else{
+            var usuario:Usuario? = jsonParse(response)
+            if (usuario != null){
+                Log.i("User","User: ${usuario.nombre}")
+            }
+            else{
+                Log.i("Error Parse","errorP Parseee!!!")
+            }
+
+
+        }
 
 
     }
 
-    fun jsonParse(data:String){
-
-        val usuarioParseado = Klaxon()
-            .parseArray<Usuario>(data)
-
-        try {
+    fun jsonParse(data:String):Usuario?{
+        try{
             val usuarioParseado = Klaxon()
                 .parseArray<Usuario>(data)
 
-            usuarioParseado?.forEach {
+            return usuarioParseado?.component1()
 
-                Log.i(
-                    "http",
-                    "Nombre ${it.nombre}"
-                )
-
-                Log.i(
-                    "http",
-                    "Id ${it.id}"
-                )
-
-                Log.i(
-                    "http",
-                    "Fecha ${it.fechaCreacion}"
-                )
-
-                it.rolesDeUsuario.forEach {
-                    Log.i(
-                        "http",
-                        "Nombre ${it.fkRol}"
-                    )
-                    Log.i(
-                        "http",
-                        "FK ${it.fkUsuario}"
-                    )
-                }
-
-            }
-        } catch (e: Exception) {
-            Log.i("http", "${e.message}")
-            Log.i(
-                "http",
-                "Error instanciando el Usuario"
-            )
         }
+        catch(err: Exception){
+            Log.i("Exception","Exception")
+
+        }
+
+        return null
     }
 
 
